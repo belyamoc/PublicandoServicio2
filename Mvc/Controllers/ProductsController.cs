24b0963d73d65f8a9ad736ad0e0ee76599812a4e
belyamoc <70119557+belyamoc@users.noreply.github.com>
2020-10-26 02:18:13 -0600

    Initial commit

---
+++ Mvc/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using Mvc.Models;
+
+namespace Mvc.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            IEnumerable<mvcProductsModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Products").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<mvcProductsModel>>().Result;
+            return View(empList);
+        }
+    }
+}
